99358cda692e33454d60059137cf587e63134eb5
DaehyeonKim <Daehyeonoff@gmail.com>
2022-03-12 00:03:41 +0900

    20220312

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
+import {React, useEffect, useState} from 'react';
+
 import './App.css';
 
 function App() {
+   useEffect(() => {
+     getServerData();
+   },[]);
+
+  const [data, setDate] = useState(""); 
+
+  const getServerData = async() => {
+    await fetch('http://localhost:3001', {
+      headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+     },
+      method: 'Get',
+    })
+    .then(res => {
+      console.log(res);
+      console.log(typeof(res));
+      return res.json();
+    })
+    .then(res => setDate(res.text));
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data}
     </div>
   );
 }
